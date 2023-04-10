edb653ce1e7e0399bbb78fddba7a9a9613084f9f
Anderson Costa <anderson.costa@levva.io>
2023-04-10 18:08:44 -0300

    feat: add implematation services

---
+++ CleanArchMvc/CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entites;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private  IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository= productRepository ?? throw new ArgumentException(nameof(productRepository));
+            _mapper= mapper;
+        }
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateProductAsync(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductById(int? id)
+        {
+            var productEntity = await _productRepository.GetProductByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _productRepository.GetProductCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetProductsAsync();
+            return  _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetProductByIdAsync(id).Result;
+            await _productRepository.RemoveProductAsync(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+            await _productRepository.UpdateProductAsync(productEntity);
+        }
+    }
+}
--- CleanArchMvc/CleanArchMvc.WebUi/Startup.cs
+++ CleanArchMvc/CleanArchMvc.WebUi/Startup.cs
@@ -38,7 +38,7 @@ namespace WebApplication1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
