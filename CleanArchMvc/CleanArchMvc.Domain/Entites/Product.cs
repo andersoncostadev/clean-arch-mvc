f1d5c72415779d5f5f04c8a3091809cafee61bd2
Anderson Costa <anderson.costa@levva.io>
2023-04-18 11:56:44 -0300

    fix: parameters for product entity

--- CleanArchMvc/CleanArchMvc.Domain/Entites/Product.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entites/Product.cs
@@ -16,14 +16,14 @@ namespace CleanArchMvc.Domain.Entites
             validateDomain(name, description, price, stock, image);
         }
 
-        public string Name { get; private set; }
-        public string Description { get; private set; }
+        public string? Name { get; private set; }
+        public string? Description { get; private set; }
         public decimal Price { get; private set; }
         public int Stock { get; private set; }
-        public string Image { get; private set; }
+        public string? Image { get; private set; }
 
         public int  CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
         private void validateDomain(string name, string description, decimal price, int stock, string image)
         {
