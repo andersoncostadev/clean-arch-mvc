19a349bb096624955ef400d1f2b068e62f0d7376
Anderson Costa <anderson.costa@levva.io>
2023-04-19 10:45:30 -0300

    feat: Implementing JWT Authentication

--- CleanArchMvc/CleanArchMvc.API/Startup.cs
+++ CleanArchMvc/CleanArchMvc.API/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace CleanArchMvc.API
 {
@@ -17,18 +16,14 @@ namespace CleanArchMvc.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructureApi(Configuration);
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchMvc.API", Version = "v1" });
-            });
+            services.AddInfrastrutureJWT(Configuration);
+            services.AddInfrastrutureSwagger();
+            services.AddControllers();    
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,9 +34,9 @@ namespace CleanArchMvc.API
             }
 
             app.UseHttpsRedirection();
-
+            app.UseStatusCodePages();
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
